932794222f0d2343816b3de0c33329e32671a997
Muhammad <azamsaeed43@gmail.com>
2025-02-19 00:55:24 +0500

    Done Html img Tag using JS in Flutter Web

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'dart:js' as js;
 
 /// Entrypoint of the application.
 void main() {
@@ -25,50 +28,178 @@ class HomePage extends StatefulWidget {
 
 /// State of a [HomePage].
 class _HomePageState extends State<HomePage> {
+  var renderOverlay = true;
+  var visible = true;
+  var switchLabelPosition = false;
+  var extend = false;
+  var mini = false;
+  var rmicons = false;
+  var customDialRoot = false;
+  var closeManually = false;
+  var useRAnimation = true;
+  var isDialOpen = ValueNotifier<bool>(false);
+  var speedDialDirection = SpeedDialDirection.up;
+  var buttonSize = const Size(56.0, 56.0);
+  var childrenButtonSize = const Size(56.0, 56.0);
+
+  String? imageUrl;
+  bool isFullScreenImage = false;
+  final TextEditingController urlController = TextEditingController();
+
+  void toggleImageFullScreen() {
+    js.context.callMethod("toggleFullScreenImage", [imageUrl]);
+  }
+
   @override
   Widget build(BuildContext context) {
+    String htmlContent = """
+  <div style="width: 100%; height: 100%; display: flex; align-items: center; justify-content: center; overflow: hidden;">
+    <img src="$imageUrl" alt="Image">
+  </div>
+""";
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Expanded(
-              child: AspectRatio(
-                aspectRatio: 1,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.circular(12),
-                  ),
+      body: imageUrl != null && isFullScreenImage
+          ? SizedBox(
+              height: double.infinity,
+              width: double.infinity,
+              child: SingleChildScrollView(
+                child: Html(
+                  data: htmlContent,
+                  style: {
+                    "div": Style(
+                      width: Width(100, Unit.auto),
+                      height: Height(100, Unit.auto),
+                      display: Display.block,
+                      alignment: Alignment.center,
+                    ),
+                  },
                 ),
               ),
-            ),
-            const SizedBox(height: 8),
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(hintText: 'Image URL'),
+            )
+          : Padding(
+              padding: const EdgeInsets.fromLTRB(32, 16, 32, 16),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Expanded(
+                    child: imageUrl != null
+                        ? InkWell(
+                            onDoubleTap: toggleImageFullScreen,
+                            child: Html(
+                              data: htmlContent,
+                              shrinkWrap: true,
+                              style: {
+                                "div": Style(
+                                  width: Width(500, Unit.percent), // Full width
+                                  height:
+                                      Height(500, Unit.percent), // Full height
+                                  display: Display.block,
+                                  alignment: Alignment.center,
+                                ),
+                              },
+                            ),
+                          )
+                        : AspectRatio(
+                            aspectRatio: 1,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.grey,
+                                borderRadius: BorderRadius.circular(12),
+                              ),
+                            ),
+                          ),
                   ),
-                ),
-                ElevatedButton(
-                  onPressed: null,
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 12, 0, 12),
-                    child: Icon(Icons.arrow_forward),
+                  const SizedBox(height: 8),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: urlController,
+                          decoration: InputDecoration(hintText: 'Image URL'),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            imageUrl = urlController.value.text.toString();
+                          });
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 12, 0, 12),
+                          child: Icon(Icons.arrow_forward),
+                        ),
+                      ),
+                    ],
                   ),
-                ),
-              ],
+                  const SizedBox(height: 64),
+                ],
+              ),
             ),
-            const SizedBox(height: 64),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: null,
-        child: Icon(Icons.add),
+      floatingActionButton: SpeedDial(
+        icon: Icons.add,
+        activeIcon: Icons.close,
+        spacing: 3,
+        mini: mini,
+        openCloseDial: isDialOpen,
+        childPadding: const EdgeInsets.all(5),
+        spaceBetweenChildren: 4,
+        dialRoot: customDialRoot
+            ? (ctx, open, toggleChildren) {
+                return ElevatedButton(
+                  onPressed: toggleChildren,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue[900],
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 22, vertical: 18),
+                  ),
+                  child: const Text(
+                    "Custom Dial Root",
+                    style: TextStyle(fontSize: 17),
+                  ),
+                );
+              }
+            : null,
+        buttonSize: buttonSize,
+        label: extend ? const Text("Open") : null,
+        activeLabel: extend ? const Text("Close") : null,
+        childrenButtonSize: childrenButtonSize,
+        visible: visible,
+        direction: speedDialDirection,
+        switchLabelPosition: switchLabelPosition,
+        closeManually: closeManually,
+        renderOverlay: renderOverlay,
+        useRotationAnimation: useRAnimation,
+        elevation: 8.0,
+        animationCurve: Curves.elasticInOut,
+        isOpenOnStart: false,
+        shape: customDialRoot
+            ? const RoundedRectangleBorder()
+            : const StadiumBorder(),
+        children: [
+          SpeedDialChild(
+            child: const Icon(Icons.fullscreen),
+            backgroundColor: Colors.red,
+            foregroundColor: Colors.white,
+            label: 'Enter fullscreen',
+            onTap: () {
+              setState(() {
+                isFullScreenImage = true;
+              });
+            },
+          ),
+          SpeedDialChild(
+            child: const Icon(Icons.fullscreen_exit),
+            backgroundColor: Colors.deepOrange,
+            foregroundColor: Colors.white,
+            label: 'Exit fullscreen',
+            onTap: () {
+              setState(() {
+                isFullScreenImage = false;
+              });
+            },
+          ),
+        ],
       ),
     );
   }

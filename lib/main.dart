b8e29f13ba1a3ab40a517a44ff6e79f3aa40282a
Muhammad <azamsaeed43@gmail.com>
2025-02-19 01:22:50 +0500

    Update Title

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Flutter Demo', home: const HomePage());
+    return MaterialApp(title: 'Flutter Task', home: const HomePage());
   }
 }
 
